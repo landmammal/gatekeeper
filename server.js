40a8242544230f04c5a35a2bec6626fb12a8f459
landmammal <evolpirate@gmail.com>
2020-04-15 22:10:02 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import http from "http"; // http  module to set up server accepting https requests
+
+// importing express app setup
+const app = require("./src/app");
+
+const port = process.env.PORT || 3000;
+
+// passing express app to handle all request
+const server = http.createServer(app);
+
+server.listen(port);
+console.log("server is live on port: " + port);
