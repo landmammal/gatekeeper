167e12f471e86601d1b92a2f8352887dec490531
landmammal <evolpirate@gmail.com>
2020-06-05 23:47:11 -0500

    updating user spec

--- spec/users.test.js
+++ spec/users.test.js
@@ -1,36 +1,38 @@
-var chai = require("chai");
-var chaiHttp = require("chai-http");
-var server = require("../server");
-var User = require("../src/models/user");
+require("dotenv/config");
+const server = require("../server");
+// const User = require("../src/models/user");
+const chai = require("chai");
 const jwt = require("jsonwebtoken");
+const chaiHttp = require("chai-http");
 
-var expect = chai.expect;
+const expect = chai.expect;
 
 // Setting up the chai http plugin
 chai.use(chaiHttp);
 
-var request;
+let request;
 
-describe("POST /api/register-user", function () {
+describe("POST /api/register", function () {
   beforeEach(function () {
     request = chai.request(server);
   });
 
   it("should register a user", (done) => {
     // test user
-    const user = new User({
+    const user = {
       fullname: "First Example",
       email: "First@Example.com",
       password: "test123",
-    });
+      confirmPassword: "test123",
+    };
 
     // api call
     request
-      .post("/api/register-user")
+      .post("/api/register")
       .send(user)
       .end(function (err, res) {
-        var responseStatus = res.status;
-        var responseBody = res.body;
+        const responseStatus = res.status;
+        const responseBody = res.body;
         user.token = "123.112.123";
 
         // Run assertions on the response
@@ -38,7 +40,15 @@ describe("POST /api/register-user", function () {
 
         expect(responseStatus).to.equal(200);
 
-        expect(responseBody).to.be.an("object").that.includes(user);
+        // expect(responseBody).to.be.an("object").that.includes(user.fullname);
+        expect(responseBody.user).to.have.own.property(
+          "email",
+          "First@Example.com"
+        );
+        expect(responseBody.user).to.have.own.property(
+          "fullname",
+          "First Example"
+        );
 
         // The `done` function is used to end any asynchronous tests
         done();
@@ -47,19 +57,20 @@ describe("POST /api/register-user", function () {
 
   it("should save users password to db as a  hash", (done) => {
     // test user
-    const user = new User({
+    const user = {
       fullname: "First Example",
       email: "First@Example.com",
       password: "test123",
-    });
+      confirmPassword: "test123",
+    };
 
     // api call
     request
-      .post("/api/register-user")
+      .post("/api/register")
       .send(user)
       .end(function (err, res) {
-        var responseStatus = res.status;
-        var responseBody = res.body;
+        const responseStatus = res.status;
+        const responseBody = res.body;
         user.token = "123.112.123";
 
         // Run assertions on the response
@@ -67,106 +78,77 @@ describe("POST /api/register-user", function () {
 
         expect(responseStatus).to.equal(200);
 
-        expect(responseBody.password).to.not.have.string("test123");
+        expect(responseBody.user.password).to.not.have.string("test123");
 
         // The `done` function is used to end any asynchronous tests
         done();
-      });
-  });
-
-  it("should reject users with duplicate emails", (done) => {
-    // Test User
-    const user = new User({
-      fullname: "First Example",
-      email: "First@Example.com",
-      password: "test123",
-    });
-
-    request
-      .post("/api/register-user")
-      .send(user)
-      .end(function (err, res) {
-        request
-          .post("/api/register-user")
-          .send(user)
-          .end(function (err, res) {
-            var responseStatus = res.status;
-
-            // Run assertions on the response
-
-            expect({ err }).to.have.string(
-              "There is a user with that email try login in or  going to forgot password",
-              "no duplicate emails"
-            );
-
-            expect(responseStatus).to.equal(403);
-
-            // The `done` function is used to end any asynchronous tests
-            done();
-          });
       });
   });
 
   it("should generate a jwt token", (done) => {
     // test user
-    const user = new User({
+    const user = {
       fullname: "First Example",
       email: "First@Example.com",
       password: "test123",
-    });
+      confirmPassword: "test123",
+    };
 
     // api call
     request
-      .post("/api/register-user")
+      .post("/api/register")
       .send(user)
       .end(function (err, res) {
-        var responseStatus = res.status;
-        var responseBody = res.body;
+        const responseStatus = res.status;
+        const responseBody = res.body;
 
         // Run assertions on the response
         expect(err).to.be.null;
 
         expect(responseStatus).to.equal(200);
 
-        expect(responseBody.token.split(".")).to.have.lengthOf(2);
+        expect(responseBody.token.split(".")).to.have.lengthOf(3);
 
-        expect(responseBody).to.be.an("object").that.includes(user);
+        expect(responseBody.user).to.be.an("object").that.includes({
+          fullname: "First Example",
+          email: "First@Example.com",
+        });
 
         // The `done` function is used to end any asynchronous tests
         done();
       });
   });
 
-  it("should return the user encrypted in JWT token", (done) => {
+  it("should return the user ID encrypted in JWT token", (done) => {
     // test user
-    const user = new User({
+    const user = {
       fullname: "First Example",
       email: "First@Example.com",
       password: "test123",
-    });
+      confirmPassword: "test123",
+    };
 
     // api call
     request
-      .post("/api/register-user")
+      .post("/api/register")
       .send(user)
       .end(function (err, res) {
-        let jwtToken;
-        let responseStatus = res.status;
-        let responseBody = res.body;
+        const responseStatus = res.status;
+        const responseBody = res.body;
 
         // Run assertions on the response
         expect(err).to.be.null;
 
         expect(responseStatus).to.equal(200);
 
-        jwt.sign({ user }, "secretkey", { expiresIn: "30s" }, (err, token) => {
-          jwtToken = token;
+        jwt.verify(responseBody.token.trim(), "secret", (err, userData) => {
+          if (err) throw err;
+
+          expect(userData.payload.email).to.equal("First@Example.com");
+          expect(userData.payload.id).to.be.a("string");
+          // The `done` function is used to end any asynchronous tests
+          done();
         });
-
-        expect(responseBody.token).to.equal(jwtToken);
-
-        // The `done` function is used to end any asynchronous tests
-        done();
       });
   });
 });
